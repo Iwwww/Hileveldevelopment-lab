01351bd1ee16a3e5922473e9deb7ff55587f8e81
TheEmpireMan <45224503+Iwwww@users.noreply.github.com>
2022-03-14 22:59:38 +0300

    Initial commit

---
+++ src/User.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+
+class User {
+    public:
+
+    private:
+        std::string* m_name;
+        std::string* m_sername;
+        std::string* m_login;
+        std::string* m_password;
+};
---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cmath>
+#include "CMenu/CMenu.cpp"
+#include "CMenu/CMenuItem.cpp"
+
+#pragma region функции-заглушки
+int f1() {
+    std::cout << "sqrt(25) = " << sqrt(25);
+    return 1;
+}
+
+int f2() {
+    for (int i = 1; i < 8; i++) {
+        for (int j = 1; j < i; j++) { 
+            std::cout << "* ";
+        }
+    std::cout << "\n";
+    }
+    std::cout << "function f2 is running...\n\n";
+    return 2;
+}
+
+int f3() {
+    std::cout << "function f3 is running...\n\n";
+    return 3;
+}
+
+int f4() {
+    std::cout << "function f4 is running...\n\n";
+    return 4;
+}
+
+int f5() {
+    std::cout << "function f5 is running...\n\n";
+    return 5;
+}
+#pragma endregion функции-заглушки
+
+const int items_number = 5;
+
+int main() {
+    using namespace YMM;
+
+    CMenuItem items[items_number] {
+        CMenuItem{"first item", f1},
+        CMenuItem{"second item", f2},
+        CMenuItem{"third item", f3},
+        CMenuItem{"fourth item", f4},
+        CMenuItem{"fives item", f5}
+    };
+
+    CMenu menu("My console menu", items, items_number);
+    while (menu.runCommand()){};
+
+    return 0;
+}
