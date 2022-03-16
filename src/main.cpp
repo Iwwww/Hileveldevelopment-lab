fcbb02f9e6a7dd573fa8064205159d88677e0acf
Iwwww <mike.yolgin@gmail.com>
2022-03-16 22:24:08 +0300

    Change included files from *.cpp to *.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cmath>
-#include "CMenu/CMenu.cpp"
-#include "CMenu/CMenuItem.cpp"
+#include "CMenu/CMenu.h"
+#include "CMenu/CMenuItem.h"
 
 #pragma region функции-заглушки
 int f1() {
@@ -42,11 +42,11 @@ int main() {
     using namespace YMM;
 
     CMenuItem items[items_number] {
-        CMenuItem{"first item", f1},
-        CMenuItem{"second item", f2},
-        CMenuItem{"third item", f3},
-        CMenuItem{"fourth item", f4},
-        CMenuItem{"fives item", f5}
+        CMenuItem("first item", f1),
+        CMenuItem("second item", f2),
+        CMenuItem("third item", f3),
+        CMenuItem("fourth item", f4),
+        CMenuItem("fives item", f5)
     };
 
     CMenu menu("My console menu", items, items_number);
