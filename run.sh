2bc86cbc9b95dca20778e51b1de5a37dd56cb037
Iwwww <mike.yolgin@gmail.com>
2022-04-17 23:12:57 +0300

    Added run bash script

---
+++ run.sh
@@ -0,0 +1,5 @@
+cd src/
+g++ main.cpp CMenu/CMenu.cpp CMenu/CMenuItem.cpp Models/User.cpp Models/Product.cpp Models/Employer/Employer.cpp Models/Provider/Provider.cpp
+chmod +x a.out
+echo '<<< END COMPILATION >>>'
+./a.out
