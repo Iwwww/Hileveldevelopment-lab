04cdc1e41b4f285dd8e27f92fa1794c82b75c293
Iwwww <mike.yolgin@gmail.com>
2022-04-18 22:14:10 +0300

    Added output flag to gcc

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 cd src/
-g++ main.cpp CMenu/CMenu.cpp CMenu/CMenuItem.cpp Models/User.cpp Models/Product.cpp Models/Employer/Employer.cpp Models/Provider/Provider.cpp
+g++ main.cpp CMenu/CMenu.cpp CMenu/CMenuItem.cpp Models/User.cpp Models/Product.cpp Models/Employer/Employer.cpp Models/Provider/Provider.cpp -o ../a.out
 chmod +x a.out
 echo '<<< END COMPILATION >>>'
+cd ..
 ./a.out
