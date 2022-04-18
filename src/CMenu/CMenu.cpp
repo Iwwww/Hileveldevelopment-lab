02e048451580f69249441ee0c407258167320231
Iwwww <mike.yolgin@gmail.com>
2022-04-18 22:16:27 +0300

    Added safe console input

--- src/CMenu/CMenu.cpp
+++ src/CMenu/CMenu.cpp
@@ -32,8 +32,11 @@ namespace YMM {
 
     int CMenu::runCommand() {
         print();
-        std::cout << "\n Select >> ";
-        std::cin >> m_select;
+        do {
+            std::cout << "\n Select >> ";
+            std::cin >> m_select;
+        } while (m_select == -1 || m_select > m_count);
+        
         return m_items[m_select - 1].run();
     }
 }
